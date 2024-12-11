5c4192f505a7d92899b6ac2a83e744bc7c82fd2c
ShestopalovKonstantin <yarkinadaryaa@gmail.com>
2024-12-11 21:03:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print(5*9**0.5)#1st program
+print(9.99>9.98 and 1000!=100.1)#2nd program
+print(2*2+2)#3rd program
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+print(int(float('123.456') * 10) % 10)#4th program
